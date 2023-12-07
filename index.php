51725d06daf9dbfc25a7ec64ed559e984cb43d3d
Mahdi-Hasan2009 <153179959+Mahdi-Hasan2009@users.noreply.github.com>
2023-12-07 15:46:23 +0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,27 @@
+<DOCTYPE!html>
+    <title>
+        PHP
+    </title>
+<br>
+<head>PHP Class</head>
+<br>
+<body>
+    <h1>
+        Hello <?php echo"Mahdi";?>
+    </h1>
+</body>
+</html>
+<?php 
+$name = "Mahdi";
+$mahdi = "name";
+echo"Hello $name";
+echo"$mahdi";
+echo "<DOCTYPE!html>
+<br>
+<body>
+<h1>
+Hello World.
+</h1>
+</body>
+</html>";
+?>
